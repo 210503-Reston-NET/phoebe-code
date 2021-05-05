79cf05512505623efe2810eff74482993c19bde0
Phoebe <jia.520@outlook.com>
2021-05-04 17:48:35 -0700

    begun the Restaurant Review program

---
+++ 01_csharp/RestaurantReviews/RRModels/Restaurant.cs
@@ -0,0 +1,21 @@
+namespace RRModels
+{
+    public class Restaurant
+    {
+        public Restaurant() {}
+        public Restaurant(string name, string city, string state)
+        {
+            this.Name = name;
+            this.City = city;
+            this.State = state;
+        }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public override string ToString()
+        {
+            return $" Name: {Name} \n Location: {City}, {State}";
+        }
+    }
+}
---
+++ 01_csharp/RestaurantReviews/RRUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RRModels;
+
+namespace RRUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Restaurant sushiLand = new Restaurant("sushi land", "Seattle", "WA");
+            Console.WriteLine(sushiLand.ToString());
+        }
+    }
+}
